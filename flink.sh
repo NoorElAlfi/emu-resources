eaeb82638813cee09d8a8497bca45c540fab4f5f
NoorElAlfi <noor.alfi@gmail.com>
2024-03-13 18:27:50 -0400

    Update Install scripts

--- flink.sh
+++ flink.sh
@@ -4,4 +4,5 @@ sudo apt-get install openjdk-8-jdk
 wget 'https://archive.apache.org/dist/flink/flink-1.11.2/flink-1.11.2-bin-scala_2.11.tgz'
 tar zxvf flink-1.11.2-bin-scala_2.11.tgz
 cd flink-1.11.2/ || exit
-./bin/start-cluster.sh
+sudo ./bin/start-cluster.sh
+exit
