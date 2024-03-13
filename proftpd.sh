42f382b748c133dadaed3b6add0e43a2c975186d
NoorElAlfi <noor.alfi@gmail.com>
2024-03-13 18:16:53 -0400

    Update Install scripts

--- proftpd.sh
+++ proftpd.sh
@@ -8,6 +8,7 @@ cd proftpd-1.3.5 || exit
 make 
 sudo make install
 sudo ./proftpd -n -c "/home/noor/proftpd-1.3.5/sample-configurations/basic.conf"
-sudo apt install php apache2 sudo mkdir /home/var/www/html/test
+sudo apt install php apache2 
+sudo mkdir /home/var/www/html/test
 sudo chmod 777 /var/www/html/test
 exit
