fc2605af6be0faf9cf4c4e4c1cd0748f0d384425
NoorElAlfi <noor.alfi@gmail.com>
2024-03-22 14:51:24 -0400

    Install action files addition/updates

--- tc.sh
+++ tc.sh
@@ -2,8 +2,8 @@
 sudo apt-get update;
 sudo apt-get install -y tomcat8 tomcat8-admin;
 sudo head -n -1 /etc/tomcat8/tomcat-users.xml > /etc/tomcat8/temp.xml;
-printf '<role rolename=\"manager-gui\"/>\n' >> /etc/tomcat8/temp.xml;
-printf '<user username=\"tomcat\" password=\"tomcat\" roles=\"manager-gui\"/>\n' >> /etc/tomcat8/temp.xml;
+printf '<role rolename="manager-gui"/>\n' >> /etc/tomcat8/temp.xml;
+printf '<user username="tomcat" password="tomcat" roles="manager-gui"/>\n' >> /etc/tomcat8/temp.xml;
 printf '</tomcat-users>\n' >> /etc/tomcat8/temp.xml;
 sudo rm -f /etc/tomcat8/tomcat-users.xml;
 sudo mv /etc/tomcat8/temp.xml /etc/tomcat8/tomcat-users.xml;
