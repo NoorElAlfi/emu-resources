e8ac117d24784465c4f05417df51da85af730db9
NoorElAlfi <noor.alfi@gmail.com>
2024-03-25 16:00:33 -0400

    Install action files addition/updates

--- proftpd.sh
+++ proftpd.sh
@@ -7,6 +7,8 @@ cd proftpd-1.3.5;
 ./configure --with-modules=mod_copy;
 make;
 sudo make install;
+sudo mkdir /var/www/html/test;
+sudo chmod 777 /var/www/html/test;
 sudo sh /home/noor/emu-resources/apache.sh
 sudo ./proftpd -n -c "/home/noor/proftpd-1.3.5/sample-configurations/basic.conf";
 exit;
