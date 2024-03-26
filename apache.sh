da5de6a6b6da5ff06060c02aad136748a845aa4f
NoorElAlfi <noor.alfi@gmail.com>
2024-03-26 19:02:56 -0400

    Install action files addition/updates

--- apache.sh
+++ apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install -y php apache2; 
-sudo mkdir /home/var/www/html/test;
+sudo mkdir /var/www/html/test;
 sudo chmod 777 /var/www/html/test;
 exit;
--- flink.sh
+++ flink.sh
@@ -4,5 +4,5 @@ sudo apt-get install -y openjdk-8-jdk;
 wget https://archive.apache.org/dist/flink/flink-1.11.2/flink-1.11.2-bin-scala_2.11.tgz;
 tar zxvf flink-1.11.2-bin-scala_2.11.tgz;
 cd flink-1.11.2/;
-sudo ./bin/start-cluster.sh;
+sudo .cd /bin/start-cluster.sh;
 exit;
